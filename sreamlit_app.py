71e5055d1fb1bce2e32cd7ef8e7296d080edd139
minsoo-s <alstn6754@outlook.com>
2022-11-22 13:38:28 +0900

    7

--- sreamlit_app.py
+++ sreamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title('hello')
 view=[100, 150, 30]
 view
-st.bar_chart(view)
-from PIL import Image
-img=Image.open(r'C:\Users\USER\Downloads\DownsamplingData\TrainingData\data\Images\V2F_HY_9147_20201102_081931_E_CH0_Seoul_Sun_Mainroad_Day_72590.png')
-st.image(img, caption='one')
+# st.bar_chart(view)
+# from PIL import Image
+# img=Image.open(r'C:\Users\USER\Downloads\DownsamplingData\TrainingData\data\Images\V2F_HY_9147_20201102_081931_E_CH0_Seoul_Sun_Mainroad_Day_72590.png')
+# st.image(img, caption='one')
