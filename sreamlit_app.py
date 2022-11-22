09f88cdea695ee8153d1cd380f485f38254326e7
minsoo-s <alstn6754@outlook.com>
2022-11-22 13:25:27 +0900

    6

--- sreamlit_app.py
+++ sreamlit_app.py
@@ -5,5 +5,5 @@ view=[100, 150, 30]
 view
 st.bar_chart(view)
 from PIL import Image
-img=Image.open('C:\Users\USER\Downloads\DownsamplingData\TrainingData\data\Images\V2F_HY_9147_20201102_081931_E_CH0_Seoul_Sun_Mainroad_Day_72590.png')
+img=Image.open(r'C:\Users\USER\Downloads\DownsamplingData\TrainingData\data\Images\V2F_HY_9147_20201102_081931_E_CH0_Seoul_Sun_Mainroad_Day_72590.png')
 st.image(img, caption='one')
